9f8e32d0c03227e7b0750f57c30ed13e128baacd
Peter <peterglen99@gmail.com>
2022-10-16 16:25:20 -0400

    autocheck

--- config.h
+++ config.h
@@ -118,7 +118,7 @@
 #define PACKAGE_NAME "bluepoint"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bluepoint 1.4.0"
+#define PACKAGE_STRING "bluepoint 1.0.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bluepoint"
@@ -127,7 +127,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.0"
+#define PACKAGE_VERSION "1.0.0"
 
 /* Define to 1 if all of the C90 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
@@ -135,7 +135,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.4.0"
+#define VERSION "1.0.0"
 
 /* Define to `int' if <sys/types.h> doesn't define. */
 /* #undef gid_t */
